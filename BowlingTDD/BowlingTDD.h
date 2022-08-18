db24c0c1df43d6f26ab9185eff61fb5c2fdfb551
Shabri <shabarinath.pabba@gmail.com>
2022-08-18 15:18:36 -0700

    add initial project

---
+++ BowlingTDD/BowlingTDD.h
@@ -0,0 +1,18 @@
+//
+//  BowlingTDD.h
+//  BowlingTDD
+//
+//  Created by Shabarinath Pabba on 8/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowlingTDD.
+FOUNDATION_EXPORT double BowlingTDDVersionNumber;
+
+//! Project version string for BowlingTDD.
+FOUNDATION_EXPORT const unsigned char BowlingTDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingTDD/PublicHeader.h>
+
+
