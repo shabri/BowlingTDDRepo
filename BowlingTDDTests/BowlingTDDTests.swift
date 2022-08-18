d1a3a3abcdaee69487c0b3368ddc436d18fd34c3
Shabri <shabarinath.pabba@gmail.com>
2022-08-18 15:25:41 -0700

    score rolled pins

--- BowlingTDDTests/BowlingTDDTests.swift
+++ BowlingTDDTests/BowlingTDDTests.swift
@@ -11,6 +11,7 @@ import XCTest
 class Bowling {
     private var totalScore: Int = 0
     func roll(pins: Int)  {
+        totalScore += pins
     }
     
     func score() -> Int {
@@ -22,5 +23,14 @@ class BowlingTDDTests: XCTestCase {
     func testScoreWhenNoRollsShouldBeZero() {
         let game = Bowling()
         XCTAssertEqual(0, game.score())
+    }
+    
+    func testScoreWhenYouRollSomePins() {
+        let game = Bowling()
+        game.roll(pins: 0)
+        XCTAssertEqual(0, game.score())
+        let rolledScore = 1
+        game.roll(pins: rolledScore)
+        XCTAssertEqual(rolledScore, game.score())
     }
 }
