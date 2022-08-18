dde39bc4dc93bd0ce0ba431fd242a8224674ac6d
Shabri <shabarinath.pabba@gmail.com>
2022-08-18 15:54:48 -0700

    Added a spare test with added frames

--- BowlingTDDTests/BowlingTDDTests.swift
+++ BowlingTDDTests/BowlingTDDTests.swift
@@ -11,9 +11,34 @@ import XCTest
 class Bowling {
     private var totalScore: Int = 0
     private var wasLastAStrike: Bool = false
+    private var wasLastASpare: Bool = false
+    
+    private var count: Int = 0
+    private var earlierFrameCount: Int = 0
+    
     func roll(pins: Int)  {
-        totalScore += wasLastAStrike ? pins*2 : pins
+        if pins == 10 {
+            if count % 2 == 0 {
+                count += 2
+            } else {
+                count += 1
+            }
+        } else {
+            count += 1
+        }
+        let isCountOdd = count % 2 == 1
+        totalScore += wasLastAStrike ? pins*2 : (wasLastASpare && isCountOdd ? pins*2 : pins)
         wasLastAStrike = pins == 10 ? true : false
+        if !wasLastAStrike {
+            if earlierFrameCount + pins == 10 {
+                wasLastASpare = true
+            } else {
+                wasLastASpare = false
+            }
+        } else {
+            wasLastASpare = false
+        }
+        earlierFrameCount = pins
     }
     
     func score() -> Int {
@@ -42,5 +67,16 @@ class BowlingTDDTests: XCTestCase {
             game.roll(pins: 1)
         }
         XCTAssertEqual(20, game.score())
+    }
+    
+    func testSpare() { // 9 + 1 + 2 + 2...9
+        game.roll(pins: 8)
+        game.roll(pins: 2)
+        game.roll(pins: 1) // this should be double of 1 = 2
+        game.roll(pins: 1) // finish frame 2 but no bonus
+        (1...16).forEach { _ in
+            game.roll(pins: 1)
+        }
+        XCTAssertEqual(29, game.score())
     }
 }
